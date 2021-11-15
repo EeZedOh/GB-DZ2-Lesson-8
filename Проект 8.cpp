07c2a4aeb9c5f72531d492a8b9419bd03cd0cbf4
RNDIE <57236143+EeZedOh@users.noreply.github.com>
2021-11-15 17:12:59 +0300

    Add files via upload

---
+++ Проект 8.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include "homework8.h"
+
+using namespace std;
+
+
+int main()
+{
+	//HomeWork1 ===============================================================================================
+	{
+		cout << "Task 1:\n";
+		int numerator = 10;
+		double denominator = 18;
+		try
+		{
+			if (denominator == 0) throw overflow_error("Division by zero");
+			cout << numerator << " / " << denominator << " = " << div(numerator, denominator);
+		}
+		catch (overflow_error)
+		{
+			cout << "Division by zero!";
+		}
+	}
+
+	//HomeWork2 ===============================================================================================
+	{
+		cout << "\n\nTask 2:\n";
+		Bar* bar = new Bar();
+		int n = 0;
+		try
+		{
+			bool nNotZero = true;
+			while (nNotZero)
+			{
+				cout << "Enter n: ";
+				cin >> n;
+				if (n != 0)
+				{
+					bar->set(n);
+				}
+				else nNotZero = false;
+			}
+		}
+		catch (Ex& e)
+		{
+			cout << "Exception thrown, code " << e.getX();
+		}
+		delete bar;
+	}
+
+	//HomeWork3 ===============================================================================================
+	{
+		cout << "\nTask 3:\n";
+		int fieldSize = 10;
+		Robot* robot = new Robot(fieldSize);
+
+		while (true)
+		{
+			robot->printCoordinates();
+			robot->move();
+		}
+		delete robot;
+	}
+}
